3f7c02b7ba7bdfb36ab9f72c5a0209de884be04e
森雄大 <forestocean0119@gmail.com>
2025-12-15 14:15:08 +0900

    Completed All MVP Implementation

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   experimental: {
     externalDir: true, 
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-494b37dbfc9645299042fcf51236d1fc.r2.dev',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
